1f2e3c93ad0aef34b032de515c279f900a2604b8
Artsiom Zhalkouski <azhalkouski@gmail.com>
2019-11-05 11:50:34 +0300

    extend the schema definition

--- src/index.js
+++ src/index.js
@@ -3,12 +3,31 @@ const { GraphQLServer } = require('graphql-yoga');
 const typeDefs = `
   type Query {
     info: String!
+    feed: [Link!]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `;
 
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
+
 const resolvers = {
   Query: {
-    info: () => 'This is the API of the graphql-node tutorial'
+    info: () => 'This is the API of the graphql-node tutorial',
+    feed: () => links
+  },
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url
   }
 };
 

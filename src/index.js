769af40332cf7f5282dc833d27398aec18e435e5
Artsiom Zhalkouski <azhalkouski@gmail.com>
2020-02-19 14:18:41 +0300

    resolve relation between User and Link

--- src/index.js
+++ src/index.js
@@ -1,30 +1,18 @@
 const { GraphQLServer } = require('graphql-yoga');
 const { prisma } = require('../generated/prisma-client');
 
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
 
-/**
- * context
- * The context argument is a plain JavaScript object that every resolver in the resolver chain can
- * read from and write to - it thus basically is a means for resolvers to communicate. It’s also
- * possible to already write to it at the moment when the GraphQL server itself is being
- * initialized. So, it’s also a way for you to pass arbitrary data or functions to theresolvers.
- */
+
 const resolvers = {
-  Query: {
-    // info: () => 'This is the API of the graphql-node tutorial',
-    // feed: (root, args, context, info) => {
-    //   return context.prisma.links();
-    // },
-  },
-  Mutation: {
-    // post: (root, args, context) => {
-    //   return context.prisma.createLink({
-    //     url: args.url,
-    //     description: args.description,
-    //   });
-    // }
-  }
-};
+  Query,
+  Mutation,
+  User,
+  Link
+}
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
